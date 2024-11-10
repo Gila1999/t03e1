53e2e4f42ee43d4f80a3b8de598fe976da674c51
Jose Manuel Gilabert Sabio <jgilsab99@gmail.com>
2024-11-10 18:29:51 +0100

    Ejercicio 2

--- src/main/java/com/example/t02e3/controllers/pageController.java
+++ src/main/java/com/example/t02e3/controllers/pageController.java
@@ -1,6 +1,11 @@
 package com.example.t02e3.controllers;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,12 +13,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class pageController {
     
     @GetMapping({"/", "/index", "/home"})
-    public String showHome(){
+    public String showHome(Model model){
+        // Obtenemos el año actual
+        int currentYear = LocalDate.now().getYear();
+        // Pasar el año al modelo
+        model.addAttribute("currentYear", currentYear);
         return "indexView";
     }
 
     @GetMapping("/palmares")
-    public String showPalmares() {
+    public String showPalmares(Model model) {
+        // Lista de títulos
+        List<String> titles = new ArrayList<>();
+        titles.add("Liga 45/46");
+        titles.add("Copa 34/35");
+        titles.add("Copa 39/40");
+        titles.add("Copa 48/49");
+        titles.add("Copa 06/07");
+        titles.add("Copa 09/10");
+        titles.add("Europa Legue 05/06");
+        titles.add("Europa Legue 06/07");
+        titles.add("Supercopa Europa 06/07");
+        titles.add("Supercopa España 07/08");
+        titles.add("Europa Legue 13/14");
+        titles.add("Europa Legue 14/15");
+        titles.add("Europa Legue 13/14");
+        titles.add("Europa Legue 19/20");
+        titles.add("Europa Legue 22/23");
+
+        //Pasamos la lista de títulos al modelo
+
+        model.addAttribute("titles", titles);
+        
+
         return "palmaresView";
     }
 
